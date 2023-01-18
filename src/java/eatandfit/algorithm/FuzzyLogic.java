29651130062daa3218d59b5bde53d72545dec982
theEmperorofDaiViet <Khiet.To.Development@gmail.com>
2023-01-18 20:13:24 +0700

    update FuzzyLogic class: print calculation results to console for testing purpose

--- src/java/eatandfit/algorithm/FuzzyLogic.java
+++ src/java/eatandfit/algorithm/FuzzyLogic.java
@@ -75,6 +75,12 @@ public class FuzzyLogic {
                 fuzzySetsAndMembershipValuesOfHeight.put(2, 1.0);
             }
         }
+        System.out.println("Sex: " + sex);
+        System.out.println("Height: " + height);
+        System.out.println("Fuzzy Sets and their Membership Values of Height:");
+        for(int h: this.fuzzySetsAndMembershipValuesOfHeight.keySet()){
+            System.out.println("Fuzzy Set: " + h + " - Membership Value: " + this.fuzzySetsAndMembershipValuesOfHeight.get(h));
+        }
     }
     
     public void doFuzzificationOfWeight(double weight, int sex){
@@ -118,6 +124,11 @@ public class FuzzyLogic {
                 fuzzySetsAndMembershipValuesOfWeight.put(2, 1.0);
             }
         }
+        System.out.println("Weight: " + weight);
+        System.out.println("Fuzzy Sets and their Membership Values of Weight:");
+        for(int w: this.fuzzySetsAndMembershipValuesOfWeight.keySet()){
+            System.out.println("Fuzzy Set: " + w + " - Membership Value: " + this.fuzzySetsAndMembershipValuesOfWeight.get(w));
+        }
     }
     
     public void doFuzzyInference(){
@@ -145,12 +156,34 @@ public class FuzzyLogic {
                 }
         }
         
+        System.out.println("Fuzzy Rules Table:");    
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print(FuzzyLogic.fuzzyRulesTable[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        System.out.println("Membership Values Table:");    
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print(this.membershipValuesTable[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
         // Get fuzzy output
         for(int i = 0; i < 3; i++) {
-                for(int j = 0; j < 3; j++) {
-                        this.fuzzifiedDecision[this.fuzzyRulesTable[i][j]] = Math.max(this.membershipValuesTable[i][j], this.fuzzifiedDecision[this.fuzzyRulesTable[i][j]]);
-                }
-        }        
+            for(int j = 0; j < 3; j++) {
+                    this.fuzzifiedDecision[this.fuzzyRulesTable[i][j]] = Math.max(this.membershipValuesTable[i][j], this.fuzzifiedDecision[this.fuzzyRulesTable[i][j]]);
+            }
+        }
+                
+        System.out.println("Fuzzified Decision:");
+        for(double fd: this.fuzzifiedDecision){
+            System.out.print(fd + " ");
+        }
+        System.out.println();
     }
     
     public int doDefuzzificationOfBody(){
@@ -163,6 +196,7 @@ public class FuzzyLogic {
                 this.finalDecisionOnBody = i;
             }           
         }
+        System.out.println("Final Decision on Body: " + this.finalDecisionOnBody);
         return this.finalDecisionOnBody;
     }
 }
